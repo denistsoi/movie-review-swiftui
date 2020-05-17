20693636c1c00472567f51a2d19f66956e7d8ed2
Denis Tsoi <denistsoi@gmail.com>
2020-05-17 23:22:47 +0800

    ADD: stack for movie;

--- Movie-Tracker/ContentView.swift
+++ Movie-Tracker/ContentView.swift
@@ -8,9 +8,56 @@
 
 import SwiftUI
 
+//        Button(action: {
+//            print("Hello")
+//        }) {
+//            Text("Button")
+//        }
+
+
+//ZStack {
+//    Image("code").resizable().aspectRatio(contentMode: .fit)
+//    Image(systemName: "play.fill").resizable().foregroundColor(.red)
+//        .aspectRatio(contentMode: .fit).frame(width: 100, height: 100)
+//}
 struct ContentView: View {
+    @State var title = ""
+    @State var rating = 3.0
+    @State var seen = false
+    
     var body: some View {
-        Text("Hello, World!")
+        List {
+            Section {
+                TextField("Movie Title", text: $title)
+            }
+            Section {
+                HStack {
+                    Text(String(repeating: "*", count: Int(rating))).foregroundColor(.yellow).font(.title)
+                }
+                Slider(value: $rating, in: 1...5, step: 1)
+            }
+            Section {
+                Toggle(isOn: $seen) {
+                    if title == "" {
+                        Text("I have seen this movie")
+                    } else {
+                        Text("I have seen \(title)")
+                    }
+                }
+            }
+            Section {
+                Button(action: {}) {
+                    HStack {
+                        Spacer()
+                        Text("Save")
+                        Spacer()
+                    }
+                }
+            }
+        }.listStyle(GroupedListStyle())
+
+        
+        
     }
 }
 
